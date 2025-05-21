11159ed25b3c0db881869c1a9f1c19d08212ca8c
Sudarshan Potdar <sudarshan121212@gmail.com>
2025-05-20 20:21:57 -0500

    Defined UserBooking service and updated entity classes

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,12 +1,32 @@
 package org.example.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
+    private List<User> userList;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-    public UserBookingService(User user1){
+    private static final String USERS_PATH = "../localDB/users.json";
+
+    public UserBookingService(User user1) throws IOException {
         this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
     }
 
 }
